030d076adb3b59921d450905f7b9f1269dde9bdd
Aleksandr Nokhrin <a.v.nokhrin@yandex.ru>
2026-06-08 08:12:07 +0500

    feat: инициализирован Spring Boot 4.0.0 проект с Maven, добавлены зависимости

---
+++ src/main/java/com/nokhrin/bank/BankApiApp.java
@@ -0,0 +1,11 @@
+package com.nokhrin.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApp {
+    static void main(String[] args) {
+        SpringApplication.run(BankApiApp.class, args);
+    }
+}
